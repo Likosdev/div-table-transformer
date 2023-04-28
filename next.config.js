57a764a0bba4b0e771a8ff1cf8cd318b7b56a622
Lucas Freund <info@lcsfreund.de>
2023-04-28 08:43:48 +0200

    base url

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-
-let basePath = ''
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-
-  basePath = `/${repo}`
-}
-
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  basePath: basePath,
+  basePath: 'div-table-transformer',
 }
 
 module.exports = nextConfig
