0c6359ab791d61b36ab87a538714f037aaeff7c9
Lucas Freund <info@lcsfreund.de>
2023-05-02 11:26:44 +0200

    type

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,24 +1,28 @@
+import { Router, useRouter } from 'next/router';
 import { Navbar } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
+
 const Navigation = () => {
+  const basePath = useRouter().basePath
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">LFR Tools</Navbar.Brand>
+        <Navbar.Brand href={basePath}>LFR Tools</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/div-table-transformer">Home</Nav.Link>
+            <Nav.Link href={basePath}>Home</Nav.Link>
             {/* <Nav.Link href="/About">About</Nav.Link> */}
             <NavDropdown title="Tools" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/div-table-transformer/Tools/KeywordCounter">
+              <NavDropdown.Item href={basePath + "/Tools/KeywordCounter"}>
                 Keyword Counter
               </NavDropdown.Item>
-              <NavDropdown.Item href="/div-table-transformer/Tools/TableConverter">
+              <NavDropdown.Item href={basePath + "/Tools/TableConverter"}>
                 Table Converter
               </NavDropdown.Item>
               <NavDropdown.Divider />
