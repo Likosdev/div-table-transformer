a0412745438ca81e79c25bd90d724bdd9e8a7746
Lucas Freund <info@lcsfreund.de>
2023-05-02 11:26:31 +0200

    fixed paths in navigation - navigtion is now using basepath config value

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import ConversionForm from "../components/ConversionForm";
-import ScreenSize from '../components/ScreenSize';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
